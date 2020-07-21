dac0284e4cd6c8b7a98945b8a539e1f496505102
scottstav <howdy@scotty.dance>
2020-07-21 00:44:03 -0500

    mild pog

--- bot.js
+++ bot.js
@@ -1,28 +1,38 @@
 const Discord = require('discord.js');
-
+const fetch = require('node-fetch');
 const client = new Discord.Client();
 
-
-
-client.on('ready', () => {
-
-  console.log('I am ready!');
-
-});
-
-
-
 client.on('message', message => {
-  let status = "i do not know";
-  if (message.content === 'status') {
-    fetch('https://minecraft.scotty.dance/minecraft/state')
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .then(status = data.state);
 
-    message.reply(status);
+    if (message.content === 'status') {
+	fetch('https://api.scotty.dance/minecraft/state')
+	    .catch(err => console.error(err))
+	    .then(res => res.json())
+	    .then(json => {
+	    	console.log(json);
+		if (json.state == 'stopped') {
+	    	    message.reply('The server is off, but I can \`start\` it.');
+		} else if (json.state == "running" && !json.players) {
+		    message.reply('The server should be booting up...');
+		} else if (json.players && json.players.includes("There are 0")) {
+		    message.reply('Server is on but will shutdown soon.');
+		} else if (json.players) {
+		    let players = json.players.split(":")[4];
+		    message.reply(`**Online**:\n\t${players}`);
+		}
 
-  }
+	    });
+
+    } else if (message.content === 'start') {
+	fetch('https://api.scotty.dance/minecraft/start', {method: 'POST'})
+	    .catch(err => console.error(err))
+	    .then(res => res.json())
+	    .then(json => {
+	    	console.log(json);
+	    	message.reply('Started the server, go play.');
+	    });
+
+    }
 
 });
 
